7527c373118b53764f4e11772f315eb6cf5de9be
sandyrhg <srahaingoson@gmail.com>
2019-10-24 06:34:23 +0200

    adding react config

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+        return ( 
+          <div className = "App" >
+            <Navbar dark color="primary">
+              <div className="container">
+                <NavbarBrand href="/">
+                    Ristorante Con Fusion
+                </NavbarBrand>
+              </div>
+            </Navbar>
+          </div>
+        );
   }
 }
-
 export default App;
